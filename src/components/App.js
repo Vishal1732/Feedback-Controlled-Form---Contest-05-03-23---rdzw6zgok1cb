04ed73915900c05e043ac7c573c4854927121673
Vishal Dubey <92910196+Vishal1732@users.noreply.github.com>
2023-03-05 14:23:00 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,67 +1,56 @@
-import React, { useState, useRef } from 'react';
-/**
- * @task :add validation to email, if email is not valid, if not valid email, dont allow to submit
- * @error_message :  "Email is invalid"  if email is wrong. (must be same message) 
- * 
- * 
- */
+import React, { useState } from 'react';
 
 function App() {
+  const [rating, setRating] = useState(5);
+  const [comment, setComment] = useState('');
+  const [commentError, setCommentError] = useState('');
 
- /**
-  * code here
-  */
-   const fnameRef = useRef();
-  const emailRef = useRef();
-  const [error, setError] = useState(undefined);
-  const [data, setData] = useState({ fname: undefined, lname: undefined });
+  const handleRatingChange = (event) => {
+    setRating(parseInt(event.target.value));
+  };
 
-  const change = () => {
-    // console.log(emailRef.current.value);
-    if (/^[a-zA-Z0-9]+@[a-zA-Z0-9]+\.[A-Za-z]+$/.test(emailRef.current.value)) {
-      setError(undefined);
-      document.getElementById("submit").disabled = false;
+  const handleCommentChange = (event) => {
+    setComment(event.target.value);
+    if (event.target.value.length < 5) {
+      setCommentError('Comment must be at least 5 characters');
     } else {
-      setError("Email is invalid");
-      document.getElementById("submit").disabled = true;
+      setCommentError('');
     }
   };
-        
 
-  return(
-    <div className="App">
-      <h1>How About Them Apples</h1>
-      <form onSubmit={(e) => {
-          e.preventDefault();
-          setData({
-            fname: fnameRef.current.value,
-            lname: emailRef.current.value
-          });
-        }}>
-        <fieldset>
-          <label>
-            <p>First Name</p>
-            <input id='fname' name="name"  ref={fnameRef}/>
-            <br></br>
-            <p>Email</p>
-            <input id='lname' name="name" onChange={change}  ref={emailRef}/>
-            {error && <h2 style={{color: 'red'}}>{error}</h2>}
-          </label>
-        </fieldset>
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(`Rating: ${rating}, Comment: ${comment}`);
+  };
 
-        <button id='submit' type="submit">Submit</button>
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="rating">Rating</label>
+          <input
+            id="rating"
+            type="range"
+            min="1"
+            max="10"
+            value={rating}
+            onChange={handleRatingChange}
+          />
+          <span className="rating">{rating}</span>
+        </div>
+        <div>
+          <label htmlFor="comment">Comment</label>
+          <textarea
+            id="comment"
+            value={comment}
+            onChange={handleCommentChange}
+          />
+          {commentError && <p className="comment-error">{commentError}</p>}
+        </div>
+        <button type="submit">Submit</button>
       </form>
-      {
-        data.fname != undefined && (
-          <div>
-          <h1>{data.fname}</h1>
-          <h2>{data.lname}</h2>
-          </div>
-        )
-      }
     </div>
-  )
+  );
 }
-
 
 export default App;
