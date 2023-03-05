e38b670ac9641915e99db2a28712b04189469f02
Vishal Dubey <92910196+Vishal1732@users.noreply.github.com>
2023-03-05 14:27:57 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,56 +1,62 @@
 import React, { useState } from 'react';
+import '../styles/App.css';
 
-function App() {
+const App = () => {
   const [rating, setRating] = useState(5);
   const [comment, setComment] = useState('');
   const [commentError, setCommentError] = useState('');
 
-  const handleRatingChange = (event) => {
-    setRating(parseInt(event.target.value));
+  const handleRatingChange = (e) => {
+    setRating(e.target.value);
   };
 
-  const handleCommentChange = (event) => {
-    setComment(event.target.value);
-    if (event.target.value.length < 5) {
-      setCommentError('Comment must be at least 5 characters');
+  const handleCommentChange = (e) => {
+    setComment(e.target.value);
+    if (e.target.value.length < 5) {
+      setCommentError('Comment must be atleast 5 characters.');
     } else {
       setCommentError('');
     }
   };
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    console.log(`Rating: ${rating}, Comment: ${comment}`);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (comment.length >= 5) {
+      alert(`Rating: ${rating}, Comment: ${comment}`);
+    } else {
+      setCommentError('Comment must be atleast 5 characters.');
+    }
   };
 
   return (
-    <div>
+    <div id="main">
       <form onSubmit={handleSubmit}>
         <div>
-          <label htmlFor="rating">Rating</label>
-          <input
-            id="rating"
+          <label htmlFor='rating'>Rating: </label>
+          <input 
             type="range"
             min="1"
             max="10"
+            id="rating"
             value={rating}
             onChange={handleRatingChange}
           />
-          <span className="rating">{rating}</span>
+          <span className='rating'>{rating}</span>
         </div>
         <div>
-          <label htmlFor="comment">Comment</label>
-          <textarea
-            id="comment"
+          <label htmlFor='comment'>Comment: </label>
+          <textarea 
+            id='comment'
             value={comment}
             onChange={handleCommentChange}
+            onBlur={handleCommentChange}
           />
-          {commentError && <p className="comment-error">{commentError}</p>}
+          {commentError && <p style={{ color: 'red' }} className="comment-error">{commentError}</p>}
         </div>
-        <button type="submit">Submit</button>
+        <button type='submit'>Submit</button>
       </form>
     </div>
   );
-}
+};
 
 export default App;
